d91dd1ab8910bc44efc8b8963cc1597b67568bb6
respect318 <ferid.mirzezade.education@gmail.com>
2025-07-11 21:54:14 +0400

    9-insert_school.py

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -2,11 +2,7 @@
 """
 Change school topics
 """
-
-
 from pymongo import MongoClient
-
-
 def update_topics(mongo_collection, name, topics):
     """
     Function that changes all topics of a school document based on the name:
